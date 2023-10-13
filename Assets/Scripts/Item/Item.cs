f9466e1208fbfecf0db31920d8b4925464733a27
Kaito <advestankv@gmail.com>
2023-10-13 09:59:40 +0200

    [ADD] Interactable :3

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -4,52 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Collider))]
-public class Item : MonoBehaviour
+
+public class Item : Interactable
 {
+    
     [Header("--- DATA ---")]
-    private Collider _collider;
     [SerializeField] private ItemData _itemData;
-    [SerializeField] private List<Player> _playersInRange = new();
 
-    [Header("--- EVENTS ---")]
-    [SerializeField] private UnityEvent _onPlayerEnterRange;
-    [SerializeField] private UnityEvent _onPlayerExitRange;
-
-    private void Awake()
+    protected override void OnInteract(Player player)
     {
-        _collider = GetComponent<Collider>();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<Player>() == null)
-            return;
-
-        Player p = other.GetComponent<Player>();
-        _playersInRange.Add(p);
-
-        p.PlayerController.Inputs.OnInteract.AddListener(() => PlayerInteracted(p));
-            
-        _onPlayerEnterRange?.Invoke();
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.GetComponent<Player>() == null)
-            return;
-        
-        Player p = other.GetComponent<Player>();
-        _playersInRange.Remove(p);
-        
-        p.PlayerController.Inputs.OnInteract.RemoveListener(() => PlayerInteracted(p));
-        
-        _onPlayerExitRange?.Invoke();
-    }
-
-    private void PlayerInteracted(Player player)
-    {
-        player.HoldedItem = _itemData;
+        player.HeldItem = _itemData;
         Destroy(gameObject);
     }
+    
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,19 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] private ItemData _holdedItem;
+    [FormerlySerializedAs("_holdedItem")] [SerializeField] private ItemData _heldItem;
     private PlayerController _playerController;
 
     public PlayerController PlayerController => _playerController;
 
-    public ItemData HoldedItem
+    public ItemData HeldItem
     {
-        get => _holdedItem;
-        set => _holdedItem = value;
+        get => _heldItem;
+        set => _heldItem = value;
     }
 
     private void Awake()
