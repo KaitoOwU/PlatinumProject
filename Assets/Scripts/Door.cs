74415722eb43295ffc064e8c6d07713621fa601c
BBouleDeSuiff <emaouardi@gmail.com>
2023-10-14 16:14:31 +0200

    [ADD] TP outside hub

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static GameManager;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class Door : Interactable
 {
     [SerializeField] private Transform _tpPoint;
     [SerializeField] private DoorType _doorTypeValue;
 
+    public Transform TpPoint => _tpPoint;
     public DoorType DoorTypeValue => _doorTypeValue;
     public enum DoorType
     {
@@ -17,43 +19,68 @@ public class Door : Interactable
 
     protected override void OnInteract(Player player)
     {
-        Debug.Log(gameObject.name + " interacted with");
-
-        switch (GameManager.Instance.CurrentGamePhase)
+        if(player.CurrentRoom is Hub)
         {
-            default:
-                return;
-            case GameManager.GamePhase.HUB:
-                int count = 0;
-                foreach (PlayerInfo p in GameManager.Instance.PlayerList)
-                {
-                    if(p.PlayerController.IsInteractHeld) 
-                        count++;
-                }
-                if (count == 2) // 2 POUR TEST ==> 4 !!!
-                {
-                    Hub hub = (Hub)player.CurrentRoom;
-                    GameManager.Instance.SwitchCameraState(CameraState.SPLIT);
-                    GameManager.Instance.CurrentGamePhase = GameManager.GamePhase.GAME;
+            int count = 0;
+            foreach (PlayerInfo p in GameManager.Instance.PlayerList)
+            {
+                if(p.PlayerController.IsInteractHeld) 
+                    count++;
+            }
+            if (count == 2) // 2 POUR TEST ==> 4 !!!
+            {
+                Hub hub = (Hub)player.CurrentRoom;
+                GameManager.Instance.SwitchCameraState(CameraState.SPLIT);
+                GameManager.Instance.CurrentGamePhase = GameManager.GamePhase.GAME;
 
-                    //TP All players to next room depending on the door they're interacting with (after they all hold button)
-                    hub.RoomDoorLeft.TP_Players();
-                    hub.RoomDoorRight.TP_Players();
+                //TP All players to next room depending on the door they're interacting with (after they all hold button)
+                hub.RoomDoorLeft.TP_Players(hub.RoomLeft.PreviousRoomDoor.TpPoint);
+                hub.RoomDoorRight.TP_Players(hub.RoomRight.PreviousRoomDoor.TpPoint);
 
-                    hub.RoomDoorLeft.TP_Camera(hub.RoomLeft);
-                    hub.RoomDoorRight.TP_Camera(hub.RoomRight);
-                }
-                return;
+                hub.RoomDoorLeft.TP_Camera(hub.RoomLeft);
+                hub.RoomDoorRight.TP_Camera(hub.RoomRight);
+            }
+        }
+        else
+        {
+            Room room = player.CurrentRoom;
+            switch (DoorTypeValue)
+            {
+                case DoorType.ENTRY:
+                    //SI TU RETOURNES AU HUB
+                    if(room.PreviousRoom is Hub)
+                    {
+                        Hub hub = (Hub)room.PreviousRoom;
+                        if(room.RoomSide == Hub.Side.LEFT)
+                            TP_Players(hub.RoomDoorLeft.TpPoint);
+                        else
+                            TP_Players(hub.RoomDoorRight.TpPoint);
+                        TP_Camera(room.PreviousRoom);
+                    }
+                    else
+                    {
+                        TP_Players(room.PreviousRoom.NextRoomDoor.TpPoint);
+                        TP_Camera(room.PreviousRoom);
+                    }
+                    break;
+                case DoorType.EXIT:
+                    TP_Players(room.NextRoom.PreviousRoomDoor.TpPoint);
+                    TP_Camera(room.NextRoom);
+                    break;
+            }                     
         }
     }
-    public void TP_Players()
-    {
+
+    public void TP_Players(Transform tpPoint)//TP  tous les joueurs qui intéragissent avec this porte
+    { 
+        //GARDER EN MEMOIRE LE NOMBRE DE JOUEUR POUR SAVOIR COMBIEN IL EN FAUT POUR PASSER A LA SALLE SUIVANTE DANS CHAQUE BRANCHE
         foreach(Player p in _playersInRange)
         {
-            p.gameObject.transform.position = _tpPoint.position;
+            p.gameObject.transform.position = tpPoint.position;
         }
     }
-    public void TP_Camera(Room room)
+
+    public void TP_Camera(Room room) // Est-ce que tu bouges vers une room de l'aile gauche? alors tp cam de gauche
     {
         switch (room.RoomSide)
         {
