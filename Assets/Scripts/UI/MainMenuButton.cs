79d495bc22e564cf4bc92af5149ff36f166fd1d4
Kaito <advestankv@gmail.com>
2023-11-10 16:29:57 +0100

    [ADD] New UI and anim

--- Assets/Scripts/UI/MainMenuButton.cs
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -9,20 +9,15 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class MainMenuButton : MonoBehaviour, ISelectHandler
 {
-    public Image SelectImg { get; private set; }
     private MainMenuUIManager _manager;
 
     private void Awake()
     {
-        SelectImg = GetComponentInChildren<Image>();
         _manager = GetComponentInParent<MainMenuUIManager>();
-        
-        SelectImg.transform.localScale = new Vector3(0, 1, 1);
     }
     
     public void OnSelect(BaseEventData eventData)
     {
-        _manager.DeselectAll();
-        SelectImg.transform.DOScaleX(1, 0.7f).SetEase(Ease.OutExpo);
+        _manager.Select(this);
     }
 }
--- Assets/Scripts/UI/MainMenuUIManager.cs
+++ Assets/Scripts/UI/MainMenuUIManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
@@ -11,27 +12,47 @@ using UnityEngine.UI;
 public class MainMenuUIManager : MonoBehaviour
 {
     private MainMenuButton[] _buttons;
-    [SerializeField] private Image _transition;
+    [SerializeField] private Image _transition, _selection, _logo;
+    [SerializeField] private RectTransform _firstSelected;
     [SerializeField] private EventSystem _events;
 
-private void Awake()
+    private void Awake()
     {
         _buttons = GetComponentsInChildren<MainMenuButton>();
     }
 
-    public void DeselectAll()
+    private void Start()
     {
-        _buttons.ToList().ForEach((button) =>
+        StartCoroutine(StartAnim());
+    }
+
+    IEnumerator StartAnim()
+    {
+        _events.gameObject.SetActive(false);
+        
+        yield return new WaitForSecondsRealtime(2f);
+        yield return _logo.DOColor(new(1, 1, 1, 1), 4f).WaitForCompletion();
+
+        foreach (MainMenuButton button in _buttons)
         {
-            DOTween.Kill(button.SelectImg.transform);
-            button.SelectImg.transform.DOScaleX(0, 0.2f).SetEase(Ease.OutExpo);
-        });
+            button.GetComponent<TextMeshProUGUI>().DOColor(new(1, 1, 1, 1), 3f);
+            yield return new WaitForSecondsRealtime(0.5f);
+        }
+        yield return new WaitForSecondsRealtime(1.5f);
+        yield return _selection.DOColor(new(1, 1, 1, 1), 1f).WaitForCompletion();
+        _events.gameObject.SetActive(true);
+        _events.SetSelectedGameObject(_firstSelected.gameObject);
+    }
+
+    public void Select(MainMenuButton button)
+    {
+        _selection.transform.DOMove(button.transform.position, 0.5f);
     }
 
     public void LoadLoadingScene()
     {
         _events.gameObject.SetActive(false);
-        _transition.DOColor(new Color(0, 0, 0, 1), 2f).OnComplete(() =>
+        _transition.DOColor(new Color(0, 0, 0, 1), 1.5f).OnComplete(() =>
         {
             SceneManager.LoadScene("Loading");
         });
