f851b792a5bea3f4067b1aa646972be9a39d8e3e
Emile Birraux <Emile.birraux@edu.devinci.fr>
2023-11-28 12:00:14 +0100

    [EDIT] Paintings for tandem update

--- Assets/Scripts/Puzzle/PaintSoluce.cs
+++ Assets/Scripts/Puzzle/PaintSoluce.cs
@@ -6,25 +6,17 @@ using UnityEngine.UI;
 public class PaintSoluce : MonoBehaviour
 {
     [SerializeField] private List<Sprite> _paints = new List<Sprite>();
-   [SerializeField] private List<int> _paintSelected = new List<int>();
+    [SerializeField] private int _paintSelected;
 
-    public List<int> PaintSelected { get => _paintSelected; }
-
+    public int PaintSelected { get => _paintSelected; }
+    public List<Sprite> Paints { get => _paints; }
+    private void Awake()
+    {
+        _paintSelected = Random.Range(0, _paints.Count);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        List<Sprite> paint = new List<Sprite>();
-        foreach(Sprite spr in _paints)
-        {
-            paint.Add(spr);
-        }
-        foreach (Image p in GetComponentsInChildren<Image>())
-        {
-            int rand = Random.Range(0, paint.Count);
-            p.sprite = paint[rand];
-            paint.Remove(paint[rand]);          
-            _paintSelected.Add(_paints.IndexOf(p.sprite));
-            
-        }
+        GetComponentInChildren<Image>().sprite = _paints[_paintSelected];
     }
 }
