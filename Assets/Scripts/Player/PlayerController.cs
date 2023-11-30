f84ebc6d2b1c25fa5fd01febb972e1bd42b6427b
BBouleDeSuiff <emaouardi@gmail.com>
2023-11-30 23:52:45 +0100

    [FIX] Input lock movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -144,6 +144,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (_inputManager.InputLocked)
+            return;
         if (_inputs != null)
         {
             if (_inputs.actions["Move"].ReadValue<Vector2>() != Vector2.zero)
