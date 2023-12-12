b3f223402e03a4fde7eeafa876a15f40a3b54ed5
Kaito_OwO <advestankv@gmail.com>
2023-12-12 14:59:26 +0100

    [FIX] Furniture

--- Assets/Scripts/UI/MainMenuUIManager.cs
+++ Assets/Scripts/UI/MainMenuUIManager.cs
@@ -12,9 +12,11 @@ using UnityEngine.UI;
 public class MainMenuUIManager : MonoBehaviour
 {
     private MainMenuButton[] _buttons;
-    [SerializeField] private Image _transition, _selection, _logo;
+    [SerializeField] private Button _buttonCredit;
+    [SerializeField] private Image _background, _transition, _selection, _logo;
     [SerializeField] private RectTransform _firstSelected;
     [SerializeField] private EventSystem _events;
+    [SerializeField] private CanvasGroup _mainMenu, _credits;
 
     private void Awake()
     {
@@ -29,8 +31,8 @@ public class MainMenuUIManager : MonoBehaviour
     IEnumerator StartAnim()
     {
         _events.gameObject.SetActive(false);
-        
-        yield return new WaitForSecondsRealtime(2f);
+
+        _background.DOColor(Color.white, 4f);
         yield return _logo.DOColor(new(1, 1, 1, 1), 4f).WaitForCompletion();
 
         foreach (MainMenuButton button in _buttons)
@@ -38,8 +40,7 @@ public class MainMenuUIManager : MonoBehaviour
             button.GetComponent<TextMeshProUGUI>().DOColor(new(1, 1, 1, 1), 3f);
             yield return new WaitForSecondsRealtime(0.5f);
         }
-        yield return new WaitForSecondsRealtime(1.5f);
-        yield return _selection.DOColor(new(1, 1, 1, 1), 1f).WaitForCompletion();
+        yield return _selection.DOColor(new(1, 1, 1, 1), 0.5f).WaitForCompletion();
         _events.gameObject.SetActive(true);
         _events.SetSelectedGameObject(_firstSelected.gameObject);
     }
@@ -56,5 +57,40 @@ public class MainMenuUIManager : MonoBehaviour
         {
             SceneManager.LoadScene("Loading");
         });
+    }
+
+    public void LoadCredits(bool s)
+    {
+        if (s)
+        {
+            StartCoroutine(CR_LoadCredits());
+        }
+        else
+        {
+            StartCoroutine(CR_UnloadCredits());
+        }
+    }
+
+    IEnumerator CR_LoadCredits()
+    {
+        _background.transform.DOScale(1.4f, 5f).SetEase(Ease.OutExpo);
+        yield return _mainMenu.DOFade(0f, 1f).WaitForCompletion();
+
+        _events.SetSelectedGameObject(_buttonCredit.gameObject);
+        _credits.DOFade(1f, 1f);
+    }
+    
+    IEnumerator CR_UnloadCredits()
+    {
+        yield return _credits.DOFade(1f, 1f);
+        
+        _background.transform.DOScale(1f, 5f).SetEase(Ease.OutExpo);
+        _events.SetSelectedGameObject(_buttons[0].gameObject);
+        _mainMenu.DOFade(0f, 1f).WaitForCompletion();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
