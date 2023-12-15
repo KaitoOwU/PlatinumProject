7de091ffc81471af3720f6131c040d5df056d8ad
BBouleDeSuiff <emaouardi@gmail.com>
2023-12-15 17:59:01 +0100

    [ADD] Music last minute

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -8,36 +8,42 @@ using UnityEngine.Playables;
 public class MusicManager : MonoBehaviour
 {
     private AudioSource _audioSource;
-    //[SerializeField] private AudioClip _music;
-    //[SerializeField] private AudioClip _endMusic;
+    [SerializeField] private AudioSource _audioSourceMusic;
+    [SerializeField] private AudioClip _endMusic;
 
     void Start()
     {
         UIPauseMenu.instance.OnMusicVolumeChange.AddListener(UpdateVolume);
         _audioSource = GetComponent<AudioSource>();
-        //_audioSource.clip = _music;
-        //GameManager.Instance.OnEachMinute.AddListener(StartMusic);
-        //GameManager.Instance.OnFadeMusic.AddListener(StopMusic);
+        _audioSourceMusic.clip = _endMusic;
+        _audioSourceMusic.Stop();
+        GameManager.Instance.OnEachMinute.AddListener(AnalyseMinute);
     }
 
     void UpdateVolume(float volume)
     {
         _audioSource.volume = volume;
+        _audioSourceMusic.volume = volume;
     }
 
-    //private void StartMusic()
-    //{
-    //    int count = GameManager.Instance.GameData.TimerValues.FirstPhaseTime
-    //    + GameManager.Instance.GameData.TimerValues.SecondPhaseTime
-    //    + GameManager.Instance.GameData.TimerValues.ThirdPhaseTime
-    //    - 60;
-    //    if (GameManager.Instance.Timer == count)
-    //    {
-    //        _audioSource.Play();
-    //    }
-    //}
-    //private void StopMusic()
-    //{
-    //    DOTween.To(x => _audioSource.volume = x, 1, 0, 3f);
-    //}
+    private void AnalyseMinute()
+    {
+        int count = GameManager.Instance.GameData.TimerValues.FirstPhaseTime
+        + GameManager.Instance.GameData.TimerValues.SecondPhaseTime
+        + GameManager.Instance.GameData.TimerValues.ThirdPhaseTime;
+
+        if (GameManager.Instance.Timer == 60
+            || GameManager.Instance.Timer == GameManager.Instance.GameData.TimerValues.FirstPhaseTime + 60
+            || GameManager.Instance.Timer == GameManager.Instance.GameData.TimerValues.FirstPhaseTime + GameManager.Instance.GameData.TimerValues.SecondPhaseTime + 60
+            )
+        {
+            StartMusic();
+        }
+    }
+
+    private void StartMusic()
+    {
+        _audioSourceMusic.Play();
+        DOTween.To(x => _audioSourceMusic.volume = x, 0, 1, 2f);
+    }
 }
